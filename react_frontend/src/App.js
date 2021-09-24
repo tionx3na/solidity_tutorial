257065d9c08894ad46e4abce86837aec9c84398d
xena <1runx3na2@gmail.com>
2021-09-24 13:49:05 +0530

    second fucntion interaction error

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Web3 from 'web3';
 import React, {Component} from 'react'
+import {first_contract_address, first_contract_abi} from './config'
 
 class App extends Component {
 
@@ -14,12 +15,29 @@ class App extends Component {
     const network = await web3.eth.net.getNetworkType()
     const accounts = await web3.eth.requestAccounts()
     this.setState({account: accounts[0]})
-    console.log("account", accounts[0]) 
+    const firstContract = new web3.eth.Contract(first_contract_abi, first_contract_address)
+    this.setState({firstContract})
+    const getvalue = await firstContract.methods.get().call()
+    this.setState({getvalue})
+    var value = this.state.newValue
+    if(value != '') {
+    this.firstContract.methods.set(value).send({from: this.state.account})
+    }
+  }
+
+  updateValue(evt) {
+    this.setState({
+      newValue: evt.target.value
+    })
   }
 
   constructor(props) {
     super(props);
-    this.state = {account: ''}
+    this.state = {
+      account: '',
+      getvalue: '',
+      newValue: ''
+    }
   }
 
 
@@ -30,9 +48,19 @@ class App extends Component {
           <p>
           Frontend to interact with Ethereum Smart contract.
           </p>
+          <br></br>
+          <br></br>
           <h6>
             Metamask Account: {this.state.account}
           </h6>
+          <br></br>
+          <h6>
+            Current value: {this.state.getvalue}
+          </h6>
+          <h6>
+            Enter new value: <input value = {this.state.newValue} onChange = {evt => this.updateValue(evt)}/>
+          </h6>
+          <button onClick = {this.loadBlockchainData}>Submit</button>
         </header>
       </div>
     );
---
+++ react_frontend/src/config.js
@@ -0,0 +1,34 @@
+export const first_contract_address = '0xDbF42576BAe1aeFc9Ad44f8989F01Dc57814E5c0'
+export const first_contract_abi = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_value",
+				"type": "string"
+			}
+		],
+		"name": "set",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "get",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
