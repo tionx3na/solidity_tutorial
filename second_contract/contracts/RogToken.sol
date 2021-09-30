e6390e776a9adb0f87ded236c28d148d2057857d
xena <1runx3na2@gmail.com>
2021-09-30 20:55:27 +0530

    Token name and symbol added

--- second_contract/contracts/RogToken.sol
+++ second_contract/contracts/RogToken.sol
@@ -2,14 +2,17 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract RogToken {
-
+    //Name
+    //Symbol
+    string public name = "RogToken";
+    string public symbol = "ROG";
     uint256 public totalSupply;
 
     mapping(address => uint256) public balanceOf;
 
     constructor(uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply; //1 crore tokens initialized
         //allocate the initial supply to an address
-        
     }
 }
--- second_contract/test/RogToken.js
+++ second_contract/test/RogToken.js
@@ -6,12 +6,27 @@ const RogToken = artifacts.require("./contracts/RogToken.sol");
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
 contract("RogToken", function (accounts) {
+  var tokenInstance;
+  it('initializes the contract with correct values', function() {
+    return RogToken.deployed().then(function(instance) {
+      tokenInstance = instance;
+      return tokenInstance.name();
+    }).then(function(name) {
+      assert.equal(name, 'RogToken', 'Has the correct name');
+      return tokenInstance.symbol();
+    }).then(function(symbol) {
+      assert.equal(symbol, "ROG", 'has the correct symbol');
+    })
+  })
   it("sets the total supply upon deployment", async function () {
     return RogToken.deployed().then(function(instance){
       tokenInstance = instance;
       return tokenInstance.totalSupply();
     }).then(function(totalSupply){
       assert.equal(totalSupply.toNumber(), 10000000, 'sets the total supply to 1,00,00,000');
+      return tokenInstance.balanceOf(accounts[0]);
+    }).then(function(adminBalance) {
+      assert.equal(adminBalance.toNumber(), 10000000, 'it allocates the initial supply to admin account.')
     })
   });
 });
